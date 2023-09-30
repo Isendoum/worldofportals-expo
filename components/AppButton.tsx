0ccd804ce1924bd5c1ebbc0ebc0f229bfeb34ea7
Isendoum <geotsek2003@yahoo.gr>
2023-09-30 12:14:43 +0300

    AppButton css fix

--- components/AppButton.tsx
+++ components/AppButton.tsx
@@ -67,6 +67,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     alignSelf: "center",
+    margin: 4,
     paddingVertical: 12,
     paddingHorizontal: 32,
     elevation: 3,
